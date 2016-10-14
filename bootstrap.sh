d0332189c64f69da6a33c1b21bf1d59bd49d1537
Israel Farfan <ifarfan@gmx.com>
2016-10-14 14:03:46 -0400

    New bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,35 @@
 TMP_DIR=~/.tmp_install
 GIT_REPO=https://github.com/ifarfan/prime-my-mac.git
 
-#  Download repo and run install
-[[ -d "$TMP_DIR" ]] && cd $TMP_DIR && git pull > /dev/null 2>&1 || git clone ${GIT_REPO} $TMP_DIR/
-cd $TMP_DIR && ./install_via_shell.sh
+
+function install_xcode_cli {
+  #
+  #  Tell "softwareupdate" that we were installing the CLI tool before and will attempt to continue
+  #  With help from https://github.com/timsutton/osx-vm-templates/blob/master/scripts/xcode-cli-tools.sh
+  #
+  XCODE_ERR_CODE=$(xcode-select -p > /dev/null 2>&1; echo $?)
+  status_msg "$XCODE_ERR_CODE" "XCode Command Line Tools"
+
+  if [ "$XCODE_ERR_CODE" -ne 0 ]
+  then
+    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    XCODE_CLT_VER=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
+    softwareupdate -i "$XCODE_CLT_VER" -v
+    rm -rf /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+  fi
+}
+
+
+function download_repo {
+    #  Download repo and run install
+    [[ -d "$TMP_DIR" ]] && cd $TMP_DIR && git pull > /dev/null 2>&1 || git clone ${GIT_REPO} $TMP_DIR/
+    cd $TMP_DIR && ./install.sh
+}
+
+
+function install_all {
+    install_xcode_cli
+    download_repo
+}
+
+install_all
